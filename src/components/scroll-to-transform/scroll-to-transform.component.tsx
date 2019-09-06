73b0b2e2321843c406ebc37a80635142acedf5d9
Jack Matthews <jackhkmatthews@gmail.com>
2019-09-06 14:28:51 +0100

    foo

--- src/components/scroll-to-transform/scroll-to-transform.component.tsx
+++ src/components/scroll-to-transform/scroll-to-transform.component.tsx
@@ -1,4 +1,8 @@
 import * as React from "react";
+import {
+  useClickToTransform,
+  useWheelToTransform
+} from "./scroll-to-transform.hooks";
 import * as S from "./scroll-to-transform.styles";
 import {
   IScrollToTransformProps,
@@ -14,103 +18,32 @@ export default function ScrollToTransform({
   const [deltaNumber, setDeltaNumber] = React.useState(0);
   const [translate, setTranslate] = React.useState(0);
 
-  // Use useRef for mutable variables that we want to persist
-  // without triggering a re-render on their change
-  const wheelRequestRef = React.useRef(0);
-  const clickRequestRef = React.useRef(0);
-  const lastKnownDeltaYs = React.useRef([0]);
-  const wheelTicking = React.useRef(false);
-  const clickTicking = React.useRef(false);
   const outerRef = React.useRef(document.createElement("div"));
-  const innerRef = React.useRef(document.createElement("div"));
   const outerWidth = React.useRef(0);
+  const innerRef = React.useRef(document.createElement("div"));
   const innerWidth = React.useRef(0);
   const maxTranslate = React.useRef(0);
-  const clientX = React.useRef(0);
 
   React.useEffect(() => {
     outerWidth.current = outerRef.current.clientWidth;
     innerWidth.current = innerRef.current.clientWidth;
     maxTranslate.current = innerWidth.current - outerWidth.current;
-    return () => {
-      cancelAnimationFrame(clickRequestRef.current);
-      cancelAnimationFrame(wheelRequestRef.current);
-    };
   }, []); // Make sure the effect runs only once
 
-  const handleWheel = (e: React.WheelEvent) => {
-    lastKnownDeltaYs.current.push(e.deltaY);
-    requestWheelTick();
-  };
+  const handleWheel = useWheelToTransform(
+    innerWidth,
+    translate,
+    setTranslate,
+    maxTranslate
+  );
 
-  const requestWheelTick = () => {
-    if (!wheelTicking.current) {
-      wheelRequestRef.current = requestAnimationFrame(wheelUpdate);
-    }
-    wheelTicking.current = true;
-  };
-
-  const wheelUpdate = () => {
-    wheelTicking.current = false;
-    const avgDeltaY = getAverage(lastKnownDeltaYs.current);
-    const translateChange =
-      avgDeltaY * 0.05 * 0.01 * innerRef.current.clientWidth;
-    lastKnownDeltaYs.current = [0];
-    setDeltaNumber(avgDeltaY);
-    updateTranslate(translateChange);
-    updateScrollDirection(avgDeltaY);
-  };
-
-  const getAverage = (array: number[]) => {
-    return (
-      array.reduce((previous, current) => (current += previous)) /
-      lastKnownDeltaYs.current.length
-    );
-  };
-
-  const updateTranslate = (translateChange: number): void => {
-    if (translate + translateChange > maxTranslate.current) {
-      setTranslate(maxTranslate.current);
-    } else if (translate + translateChange < 0) {
-      setTranslate(0);
-    } else {
-      setTranslate(translate + translateChange);
-    }
-  };
-
-  const updateScrollDirection = (avgDeltaY: number): void => {
-    if (
-      (avgDeltaY > -1 && scrollDirection === ScrollDirections.down) ||
-      (avgDeltaY < 1 && scrollDirection === ScrollDirections.up)
-    ) {
-      return;
-    } else if (avgDeltaY > 0) {
-      setScrollDirection(ScrollDirections.down);
-    } else {
-      setScrollDirection(ScrollDirections.up);
-    }
-  };
-
-  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    clientX.current = e.clientX;
-    requestClickTick();
-  };
-
-  const requestClickTick = () => {
-    if (!clickTicking.current) {
-      clickRequestRef.current = requestAnimationFrame(clickUpdate);
-    }
-    clickTicking.current = true;
-  };
-
-  const clickUpdate = () => {
-    clickTicking.current = false;
-    const offsetX =
-      clientX.current - innerRef.current.getBoundingClientRect().left;
-    const newTranslate = offsetX - outerWidth.current / 2;
-    const translateChange = newTranslate - translate;
-    updateTranslate(translateChange);
-  };
+  const handleClick = useClickToTransform(
+    outerWidth,
+    innerRef,
+    translate,
+    setTranslate,
+    maxTranslate
+  );
 
   return (
     <S.Outer
