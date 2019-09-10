25ac02bb2107ee268620269c3bbe77794ff0ed6e
Jack Matthews <jackhkmatthews@gmail.com>
2019-09-10 12:03:23 +0100

    feat(app): update component

--- src/components/app/app.styles.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import ClickToScroll from "../click-to-scroll/click-to-scroll";
-
-export const StyledClickToScroll = styled(ClickToScroll)`
-  width: 100%;
-`;
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,24 +1,36 @@
 import * as React from "react";
-// import { CARD_IMAGE_FILTERS } from "../../state/types";
-// import FilterAction from "../filter-action/filter-action";
+import { CARD_IMAGE_FILTERS } from "../../state/types";
+import FilterAction from "../filter-action/filter-action";
 import HorizontalScroll from "../horizontal-scroll/horizontal-scroll.component";
+import VisibleCardList from "../visible-card-list/visible-card-list";
 
 function App() {
   return (
     <div>
       <nav>
-        {/* <FilterAction filter={CARD_IMAGE_FILTERS.showAll}>
+        <FilterAction filter={CARD_IMAGE_FILTERS.showAll}>
           Show All
         </FilterAction>
-        <FilterAction filter={CARD_IMAGE_FILTERS.showKind}>
-          Show Kind
+        <FilterAction filter={CARD_IMAGE_FILTERS.showCanary}>
+          Show Canary
         </FilterAction>
-        <FilterAction filter={CARD_IMAGE_FILTERS.showEvil}>
-          Show Evil
-        </FilterAction> */}
+        <FilterAction filter={CARD_IMAGE_FILTERS.showGuatemala}>
+          Show Guatemala
+        </FilterAction>
+        <FilterAction filter={CARD_IMAGE_FILTERS.showPanama}>
+          Show Panama
+        </FilterAction>
+        <FilterAction filter={CARD_IMAGE_FILTERS.showWales}>
+          Show Wales
+        </FilterAction>
+        <FilterAction filter={CARD_IMAGE_FILTERS.showWorcester}>
+          Show Worchester
+        </FilterAction>
       </nav>
       <main>
-        <HorizontalScroll />
+        <HorizontalScroll>
+          <VisibleCardList />
+        </HorizontalScroll>
       </main>
     </div>
   );
