f5276e22a026bc6c225a50ae0bd1457cadaa6bc4
Jack Matthews <jackhkmatthews@gmail.com>
2019-09-10 12:03:45 +0100

    feat(clickToScroll): remove story

--- src/components/click-to-scroll/click-to-scroll.stories.tsx
+++ src/components/click-to-scroll/click-to-scroll.stories.tsx
@@ -1,8 +1,6 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import styled from "styled-components";
-import { initialCardsState } from "../../state/initial";
-import CardList from "../card-list/card-list";
 import ClickToScroll from "./click-to-scroll";
 
 const S = {
@@ -13,18 +11,12 @@ const S = {
   `
 };
 
-storiesOf("ClickToScroll", module)
-  .add("default", () => (
-    <S.ClickToScroll>
-      <div>
-        <h1>
-          Hell123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
-        </h1>
-      </div>
-    </S.ClickToScroll>
-  ))
-  .add("card list", () => (
-    <S.ClickToScroll>
-      <CardList cards={initialCardsState} handleCardClick={() => null} />
-    </S.ClickToScroll>
-  ));
+storiesOf("ClickToScroll", module).add("default", () => (
+  <S.ClickToScroll>
+    <div>
+      <h1>
+        Hell123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+      </h1>
+    </div>
+  </S.ClickToScroll>
+));
